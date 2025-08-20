d472d20cb1beb7c292e04e6b2e0ad375c23b4823
presidentsnow <xaviyu2005@gmail.com>
2025-08-20 20:18:35 +0200

    Some PHP exercises done.

--- exercises_php/basic_phpExercises.php
+++ exercises_php/basic_phpExercises.php
@@ -1,4 +1,49 @@
 <?php
-// Show's info about the php program
-phpinfo();
+# This's from: https://www.w3resource.com/php-exercises
+
+// #Write a PHP script to get the PHP version and configuration information.
+// phpinfo();
+
+/*
+# Write a PHP script to display the following strings.
+Sample String :
+'Tomorrow I \'ll learn PHP global variables.'
+'This is a bad command : del c:\\*.*'
+Expected Output :
+Tomorrow I 'll learn PHP global variables.
+This is a bad command : del c:\*.*
+*/
+
+// echo "Tomorrow I \'ll learn PHP global variables.\n";
+// echo "This is a bad command : del c:\\*.*\n";
+
+// #Write a PHP script to get the current file name.
+/*
+// Get the full path and filename
+$fullPath = __FILE__;
+
+// Get just the filename
+$fileName = basename(__FILE__);
+
+echo "Full path: " . $fullPath . "\n";
+echo "Filename: " . $fileName . "\n";
+*/
+
+// #Write a PHP script, which will return the following
+// #components of the url 'https://www.w3resource.com/php-exercises/php-basic-exercises.php'.
+
+// Define the URL to be parsed
+$url = 'https://www.w3resource.com/php-exercises/php-basic-exercises.php';
+
+// Parse the URL and store its components in the $url variable
+$url = parse_url(url: $url);
+
+// Display the scheme (protocol) of the parsed URL
+echo 'Scheme : ' . $url['scheme'] . "\n";
+
+// Display the host (domain) of the parsed URL
+echo 'Host : ' . $url['host'] . "\n";
+
+// Display the path of the parsed URL
+echo 'Path : ' . $url['path'] . "\n";
 ?>
--- hello_world/helloWorld.php
+++ hello_world/helloWorld.php
@@ -5,19 +5,20 @@ echo "I love $txt!\n";
 #const MYCAR = "Volvo";
 #echo MYCAR;
 
-$t = 14;
+// echo "\n";
 
-if ($t < 20) 
-{
-  echo "Have a good day!\n";
-}
+/*// The var_dump() function returns the data type and the value.
+var_dump(28);
+var_dump("Xabi");
+var_dump("Elkoroiribe Zenikaonandia");
+var_dump(3.14);
+var_dump(true);
+*/
 
-$a = 200;
-$b = 33;
-$c = 500;
+// echo "\n";
 
-if ($a > $b && $a < $c ) 
-{
-  echo "Both conditions are true";
-}
+// The PHP strrev() function reverses a string.
+$hello = "OPERA";
+$rev = "AREPO";
+echo "The word " . $hello . " and " . $rev . " is equal reversed.\n";
 ?>
